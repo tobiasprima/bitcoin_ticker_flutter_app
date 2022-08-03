d256e7a73b387dc1972bf5dfe003c7d7765682d6
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-03 10:34:53 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:bitcoin_ticker_flutter_app/price_screen';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.lightBlue,
+          scaffoldBackgroundColor: Colors.white),
+      home: Price,
+    );
+  }
+}
