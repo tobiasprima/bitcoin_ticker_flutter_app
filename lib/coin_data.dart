0f4dcb428a4260e93cdfb90fc1584fab6d46da4a
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-08 14:43:23 +0700

    Import the API

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 const List<String> currenciesList = [
   'AUD',
@@ -34,4 +34,18 @@ const List<String> cryptoList = [
 const coinAPIURL = "https://rest.coinapi.io/v1/exchangerate";
 const apiKey = "C740DADD-FBC0-4D43-899F-6570DB76D9BD";
 
-class CoinData {}
+class CoinData {
+  Future getCoinData() async {
+    String requestURL = "$coinAPIURL/BTC/USD?apikey=$apiKey";
+    http.Response response = await http.get(Uri.parse(requestURL));
+
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'Problem with the get request';
+    }
+  }
+}
